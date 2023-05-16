a81ade36d6d6e165b522330d5258961e95a8c634
Daniel Gimenes <daniel@danielgimenes.com.br>
2023-05-16 14:04:58 +0000

    Drop MVP

--- src/components/Main.tsx
+++
@@ -1,3 +0,0 @@
-import { Stack, StackProps } from "@chakra-ui/react";
-
-export const Main = (props: StackProps) => <Stack spacing={6} pt={6} px={4} {...props} />;
--- src/propHouse/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./client";
-export * from "./delegators";
-export * from "./house";
-export * from "./voters";
-export * from "./votingPower";
